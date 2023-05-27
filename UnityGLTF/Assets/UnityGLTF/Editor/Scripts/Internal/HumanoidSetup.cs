7d1b76aec71a2799ac0973192d10a4ac1803a3c8
Alberto Saldaña <asn@sonsofabit.com>
2023-05-27 09:34:13 +0200

    Retargeting bones.

--- UnityGLTF/Assets/UnityGLTF/Editor/Scripts/Internal/HumanoidSetup.cs
+++ UnityGLTF/Assets/UnityGLTF/Editor/Scripts/Internal/HumanoidSetup.cs
@@ -8,98 +8,105 @@ namespace UnityGLTF
 {
 	internal static class HumanoidSetup
 	{
-	    private static MethodInfo _SetupHumanSkeleton;
+		private static MethodInfo _SetupHumanSkeleton;
 
-	    internal static Avatar AddAvatarToGameObject(GameObject gameObject)
-	    {
-                SkinnedMeshRenderer[] skinnedMeshRenderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+		internal static Avatar AddAvatarToGameObject(GameObject gameObject)
+		{
+			SkinnedMeshRenderer[] skinnedMeshRenderers = gameObject.GetComponentsInChildren<SkinnedMeshRenderer>();
+			UnityEngine.Debug.LogError(gameObject.name + ", Meshes = " + skinnedMeshRenderers.Length);
 
-                foreach (SkinnedMeshRenderer rend in skinnedMeshRenderers) 
-                {
-                	Transform[] meshBones = rend.bones;
-	                bool[] meshBonesUsed = new bool[meshBones.Length];
-        	        BoneWeight[] weights = rend.sharedMesh.boneWeights;
-                	foreach (BoneWeight w in weights)
-                	{
-                    		if (w.weight0 != 0)
-		                        meshBonesUsed[w.boneIndex0] = true;
-                 		if (w.weight1 != 0)
-		                        meshBonesUsed[w.boneIndex1] = true;
-                    		if (w.weight2 != 0)
-                        		meshBonesUsed[w.boneIndex2] = true;
-                    		if (w.weight3 != 0)
-                        		meshBonesUsed[w.boneIndex3] = true;
-                	}
-					for (int i = 0; i < meshBones.Length; i++)
+			if (skinnedMeshRenderers.Length > 0)
+			{
+				UnityEngine.Debug.LogError("Mesh = " + skinnedMeshRenderers[0].name);
+			}
+
+			foreach (SkinnedMeshRenderer rend in skinnedMeshRenderers)
+			{
+				UnityEngine.Debug.LogError("Mesh bones = " + rend.bones.Length);
+				Transform[] meshBones = rend.bones;
+				bool[] meshBonesUsed = new bool[meshBones.Length];
+				BoneWeight[] weights = rend.sharedMesh.boneWeights;
+				foreach (BoneWeight w in weights)
+				{
+					if (w.weight0 != 0)
+						meshBonesUsed[w.boneIndex0] = true;
+					if (w.weight1 != 0)
+						meshBonesUsed[w.boneIndex1] = true;
+					if (w.weight2 != 0)
+						meshBonesUsed[w.boneIndex2] = true;
+					if (w.weight3 != 0)
+						meshBonesUsed[w.boneIndex3] = true;
+				}
+				for (int i = 0; i < meshBones.Length; i++)
+				{
+					if (meshBonesUsed[i])
 					{
-						if (meshBonesUsed[i])
-						{
-							Debug.LogError(meshBones[i].name);
-						}
-                	}
-            	}
-
-            	HumanDescription description = AvatarUtils.CreateHumanDescription(gameObject);
-				var bones = description.human;
-
-            	UnityEngine.Debug.LogError("(0) Bones " + bones.Length);
-
-            	foreach (var bone in bones)
-            	{
-                	UnityEngine.Debug.LogError(bone.humanName + "->" + bone.boneName);
-            	}
-
-            	SetupHumanSkeleton(gameObject, ref bones, out var skeletonBones, out var hasTranslationDoF);
-				description.human = bones;
-				description.skeleton = skeletonBones;
-				description.hasTranslationDoF = hasTranslationDoF;
-
-	       	 	UnityEngine.Debug.LogError("(1) Bones " + skeletonBones.Length);
-
-				foreach (var bone in skeletonBones)
-				{
-					UnityEngine.Debug.LogError(bone.name);
+						Debug.LogError(meshBones[i].name);
+					}
 				}
+			}
 
-				Avatar avatar = AvatarBuilder.BuildHumanAvatar(gameObject, description);
-				avatar.name = "Avatar";
+			HumanDescription description = AvatarUtils.CreateHumanDescription(gameObject);
+			var bones = description.human;
 
-				if (!avatar.isValid)
-				{
-					Object.DestroyImmediate(avatar);
-					return null;
-				}
+			UnityEngine.Debug.LogError("(0) Bones " + bones.Length);
 
-				var animator = gameObject.GetComponent<Animator>();
-				if (animator) animator.avatar = avatar;
-				return avatar;
-	    }
+			foreach (var bone in bones)
+			{
+				UnityEngine.Debug.LogError(bone.humanName + "->" + bone.boneName);
+			}
 
-	    private static void SetupHumanSkeleton(
-		    GameObject modelPrefab,
-		    ref HumanBone[] humanBoneMappingArray,
-		    out SkeletonBone[] skeletonBones,
-		    out bool hasTranslationDoF)
-	    {
-		    _SetupHumanSkeleton = typeof(AvatarSetupTool).GetMethod(nameof(SetupHumanSkeleton), (BindingFlags)(-1));
-		    skeletonBones = Array.Empty<SkeletonBone>();
-		    hasTranslationDoF = false;
+			SetupHumanSkeleton(gameObject, ref bones, out var skeletonBones, out var hasTranslationDoF);
+			description.human = bones;
+			description.skeleton = skeletonBones;
+			description.hasTranslationDoF = hasTranslationDoF;
 
-		    _SetupHumanSkeleton?.Invoke(null, new object[]
-		    {
-			    modelPrefab,
-			    humanBoneMappingArray,
-			    skeletonBones,
-			    hasTranslationDoF
-		    });
-	    }
+			UnityEngine.Debug.LogError("(1) Bones " + skeletonBones.Length);
+
+			foreach (var bone in skeletonBones)
+			{
+				UnityEngine.Debug.LogError(bone.name);
+			}
+
+			Avatar avatar = AvatarBuilder.BuildHumanAvatar(gameObject, description);
+			avatar.name = "Avatar";
+
+			if (!avatar.isValid)
+			{
+				Object.DestroyImmediate(avatar);
+				return null;
+			}
+
+			var animator = gameObject.GetComponent<Animator>();
+			if (animator) animator.avatar = avatar;
+			return avatar;
+		}
+
+		private static void SetupHumanSkeleton(
+			GameObject modelPrefab,
+			ref HumanBone[] humanBoneMappingArray,
+			out SkeletonBone[] skeletonBones,
+			out bool hasTranslationDoF)
+		{
+			_SetupHumanSkeleton = typeof(AvatarSetupTool).GetMethod(nameof(SetupHumanSkeleton), (BindingFlags)(-1));
+			skeletonBones = Array.Empty<SkeletonBone>();
+			hasTranslationDoF = false;
+
+			_SetupHumanSkeleton?.Invoke(null, new object[]
+			{
+				modelPrefab,
+				humanBoneMappingArray,
+				skeletonBones,
+				hasTranslationDoF
+			});
+		}
 
 
-	    // AvatarSetupTools
-	    // AvatarBuilder.BuildHumanAvatar
-	    // AvatarConfigurationStage.CreateStage
-	    // AssetImporterTabbedEditor
-	    // ModelImporterRigEditor
+		// AvatarSetupTools
+		// AvatarBuilder.BuildHumanAvatar
+		// AvatarConfigurationStage.CreateStage
+		// AssetImporterTabbedEditor
+		// ModelImporterRigEditor
 
 #if TESTING
 	    [MenuItem("Tools/Copy Hierarchy Array")]
